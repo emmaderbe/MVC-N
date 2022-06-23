2087bc9b508c266f5b7de09c51eda4feadcf90a6
emmaderbe <emma-derbe@mail.ru>
2022-06-23 13:27:43 +0300

    Initial Commit

---
+++ MVC-N/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVC-N
+//
+//  Created by Дербе Эмма on 23.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
